7546616c33585c6ebca4bc5c306c139fda08e68c
Weslley Thiago <weslleyothiago@gmail.com>
2024-11-13 09:40:22 -0300

    conteudo de acordo com o tipo da conta

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  isAdmin: boolean = false
 
-  constructor() { }
+  constructor() {
+    this.checkUserType();
+   }
 
+   checkUserType(){
+    const token = sessionStorage.getItem('access_token');
+    if (token) {
+      const decodedToken: any = jwtDecode(token);
+      this.isAdmin = decodedToken.type === 'Administrador';
+    }
+    console.log(this.isAdmin);
+   }
 }
