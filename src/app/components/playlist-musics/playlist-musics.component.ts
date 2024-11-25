4a78646875aa579638dc7c3887b373e1e7f039c0
Weslley <weslleyothiago@gmail.com>
2024-11-25 14:34:50 -0300

    Template playlist-musics

--- src/app/components/playlist-musics/playlist-musics.component.ts
+++ src/app/components/playlist-musics/playlist-musics.component.ts
@@ -3,12 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlist-musics',
   templateUrl: './playlist-musics.component.html',
-  styleUrls: ['./playlist-musics.component.scss'],
+  styleUrls: ['./playlist-musics.component.scss']
 })
-export class PlaylistMusicsComponent  implements OnInit {
+export class PlaylistMusicsComponent implements OnInit {
+  randomGradient: string = '';
 
-  constructor() { }
+  ngOnInit() {
+    this.randomGradient = this.generateRandomGradient();
+  }
 
-  ngOnInit() {}
+  private generateRandomGradient(): string {
+    const randomColor = this.getRandomColor();
+    return `linear-gradient(to right, ${randomColor}, black)`;
+  }
 
+  private getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
